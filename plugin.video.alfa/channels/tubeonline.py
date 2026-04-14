bb2b908a13929169d1edc422e96393e8b414ebc7
paeznet <jsaezu@wanadoo.es>
2026-04-14 17:52:29 +0200

    tubeonline

--- plugin.video.alfa/channels/tubeonline.py
+++ plugin.video.alfa/channels/tubeonline.py
@@ -326,8 +326,9 @@ def findvideos_matches(item, matches_int, langs, response, **AHkwargs):
     except Exception:
         pass
     
-    servers = {'strmup': 'bestb', 'hqq': '', 'asnwish': 'streamwish', 'bysejikuar': 'filemoon', 'vidnest': 'highstream'}
-    
+    servers = {'strmup': 'bestb', 'hqq': '', 'asnwish': 'streamwish',
+               'bysejikuar': 'filemoon', 'vidnest': 'highstream', 'luluvdo': 'lulustream'}
+    logger.debug(matches_int)
     for lang_container in matches_int:
         language = IDIOMAS.get(lang_container["id"], "VOSE")
         for elem in lang_container.find_all("li"):
